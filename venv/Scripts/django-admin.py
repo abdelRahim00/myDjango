27de2a58e6b057226f68f6b4ef2cd403c4040e6f
abdelRahim00 <48139474+abdelRahim00@users.noreply.github.com>
2019-12-13 00:25:52 +0200

    abdelrahimDjango

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Rahim\Desktop\4th\web projj\Django-master\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
